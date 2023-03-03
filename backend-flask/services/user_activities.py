4820fbfe81aa0d791eccaf466bbc67c91b5befff
MohanVaddella <mohanvaddella@gmail.com>
2023-03-03 09:00:32 +0000

    fix xray

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -2,18 +2,18 @@ from datetime import datetime, timedelta, timezone
 from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
-    # xray -------
-    segment = xray_recorder.begin_segment('user_activities')
+    try:
+      
     
-    model = {
-      'errors': None,
-      'data': None
-    }
+      model = {
+        'errors': None,
+        'data': None
+      }
 
-    now = datetime.now(timezone.utc).astimezone()
-    if user_handle == None or len(user_handle) < 1:
+      now = datetime.now(timezone.utc).astimezone()
+      if user_handle == None or len(user_handle) < 1:
         model['errors'] = ['blank_user_handle']
-    else:
+      else:
         now = datetime.now()
         results = [{
           'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
@@ -31,4 +31,8 @@ class UserActivities:
           "results-size": len(model['data'])
         }
         subsegment.put_metadata('key', dict, 'namespace')
-        return model
+        xray_recorder.end_subsegment()
+    finally:
+      # Close the segment
+      xray_recorder.end_subsegment()
+    return model
