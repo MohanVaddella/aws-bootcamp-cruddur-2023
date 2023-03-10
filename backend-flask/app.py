94d6fd34c15a6ee737446e22cb3338b350d0e916
MohanVaddella <mohanvaddella@gmail.com>
2023-03-10 08:57:06 +0000

    update cognito

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+import sys
+
+from flask_awscognito import AWSCognitoAuthentication
+
 
 from services.home_activities import *
 from services.notifications_activities import *
@@ -69,6 +73,7 @@ tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
+
 # X-RAY -----------
 XRayMiddleware(app, xray_recorder)
 
@@ -84,8 +89,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -154,8 +159,12 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 @xray_recorder.capture('activities_home')
+@aws_auth.authentication_required
 def data_home():
   data = HomeActivities.run()
+  claims = aws_auth.claims
+  app.logger.debug('claims')
+  app.logger.debug(claims)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
