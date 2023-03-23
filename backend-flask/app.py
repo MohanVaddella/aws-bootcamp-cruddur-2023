2761ef016800721f692138bb415b7cbcfcfc7254
MohanVaddella <mohanvaddella@gmail.com>
2023-03-23 12:58:48 +0000

    code should be runnable

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -267,11 +267,11 @@ def data_activities_reply(activity_uuid):
 def data_messages_counter():
 
   access_token = extract_access_token(request.headers)
-  try:
-    claims = cognito_jwt_token.verify(access_token)
-    cognito_user_id = claims['sub']
-    count = MessagesCounter.run(cognito_user_id=cognito_user_id)
-  return {'count': count}, 200
+  #try:
+  #  claims = cognito_jwt_token.verify(access_token)
+  #  cognito_user_id = claims['sub']
+  #  count = MessagesCounter.run(cognito_user_id=cognito_user_id)
+  #return {'count': count}, 200
 
 
 if __name__ == "__main__":
